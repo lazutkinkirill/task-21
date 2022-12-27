48247fb6c1d61e75217466aa5d4ee62eb3ec4fad
lazutkinkirill <lazutkin_k@inbox.ru>
2022-12-27 23:00:56 +0300

    finish task 21

--- 21/Program.cs
+++ 21/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.Write("Введите x1 ");
+int x1 = int.Parse(Console.ReadLine());
+Console.Write("Введите y1 ");
+int y1 = int.Parse(Console.ReadLine());
+Console.Write("Введите z1 ");
+int z1 = int.Parse(Console.ReadLine());
+Console.Write("Введите x2 ");
+int x2 = int.Parse(Console.ReadLine());
+Console.Write("Введите y2 ");
+int y2 = int.Parse(Console.ReadLine());
+Console.Write("Введите z2 ");
+int z2 = int.Parse(Console.ReadLine());
+
+double d = Math.Sqrt(Math.Pow(x1-x2, 2) + Math.Pow(y1-y2, 2) + Math.Pow(z1-z2, 2));
+
+Console.Write($"d={d:f3}");
